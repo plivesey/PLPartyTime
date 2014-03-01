6fa629272d469258ffb4a47c695b8c3ccb5baf55
Peter Livesey <plivesey453@gmail.com>
2014-03-01 12:26:06 -0800

    Added V1 of the API.

--- PLPartyTime/PLPartyTime.h
+++ PLPartyTime/PLPartyTime.h
@@ -7,7 +7,44 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+
+
+@protocol PLPartyTimeDelegate;
 
 @interface PLPartyTime : NSObject
+
+@property (nonatomic, weak) id<PLPartyTimeDelegate> delegate;
+
+@property (nonatomic, readonly, strong) NSString *serviceType;
+@property (nonatomic, readonly, strong) NSString *displayName;
+
+- (instancetype)initWithServiceType:(NSString *)serviceType;
+- (instancetype)initWithServiceType:(NSString *)serviceType
+                        displayName:(NSString *)displayName;
+
+- (void)joinParty;
+
+- (BOOL)sendData:(NSData *)data
+        withMode:(MCSessionSendDataMode)mode
+           error:(NSError **)error;
+- (BOOL)sendData:(NSData *)data
+         toPeers:(NSArray *)peerIDs
+        withMode:(MCSessionSendDataMode)mode
+           error:(NSError **)error;
+
+@end
+
+@protocol PLPartyTimeDelegate <NSObject>
+
+@required
+- (void)partyTime:(PLPartyTime *)partyTime
+   didReceiveData:(NSData *)data
+         fromPeer:(MCPeerID *)peerID;
+
+@optional
+- (void)partyTime:(PLPartyTime *)partyTime
+  connectedToPeer:(MCPeerID *)peer
+     currentPeers:(MCPeerID *)currentPeers;
 
 @end
--- PLPartyTime/PLPartyTime.m
+++ PLPartyTime/PLPartyTime.m
@@ -8,6 +8,58 @@
 
 #import "PLPartyTime.h"
 
+
+@interface PLPartyTime ()
+
+@property (nonatomic, readwrite, strong) NSString *serviceType;
+@property (nonatomic, readwrite, strong) NSString *displayName;
+
+@end
+
 @implementation PLPartyTime
+
+#pragma mark - Initialization
+
+- (instancetype)initWithServiceType:(NSString *)serviceType
+{
+  return [self initWithServiceType:serviceType
+                       displayName:[UIDevice currentDevice].name];
+}
+
+- (instancetype)initWithServiceType:(NSString *)serviceType
+                        displayName:(NSString *)displayName
+{
+  self = [super init];
+  if (self)
+  {
+    self.serviceType = serviceType;
+    self.displayName = displayName;
+  }
+  return self;
+}
+
+#pragma mark - Join
+
+- (void)joinParty
+{
+  
+}
+
+#pragma mark - Communicate
+
+- (BOOL)sendData:(NSData *)data
+        withMode:(MCSessionSendDataMode)mode
+           error:(NSError **)error
+{
+  return YES;
+}
+
+- (BOOL)sendData:(NSData *)data
+         toPeers:(NSArray *)peerIDs
+        withMode:(MCSessionSendDataMode)mode
+           error:(NSError **)error
+{
+  return YES;
+}
 
 @end
