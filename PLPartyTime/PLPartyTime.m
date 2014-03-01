4efabbfdba4968121cb7f382fb5f347b0b3608af
Peter Livesey <plivesey453@gmail.com>
2014-03-01 13:31:11 -0800

    Setting up all the properties and delegates that we need

--- PLPartyTime/PLPartyTime.m
+++ PLPartyTime/PLPartyTime.m
@@ -9,10 +9,16 @@
 #import "PLPartyTime.h"
 
 
-@interface PLPartyTime ()
+@interface PLPartyTime () <MCSessionDelegate, MCNearbyServiceAdvertiserDelegate, MCNearbyServiceBrowserDelegate>
 
+// Public Properties
 @property (nonatomic, readwrite, strong) NSString *serviceType;
 @property (nonatomic, readwrite, strong) NSString *displayName;
+
+@property (nonatomic, strong) MCSession *session;
+@property (nonatomic, strong) MCPeerID *peerID;
+@property (nonatomic, strong) MCNearbyServiceAdvertiser *advertiser;
+@property (nonatomic, strong) MCNearbyServiceBrowser *browser;
 
 @end
 
@@ -61,5 +67,123 @@
 {
   return YES;
 }
+
+#pragma mark - Properties
+
+- (MCSession *)session
+{
+  if (!_session)
+  {
+    _session = [[MCSession alloc] initWithPeer:self.peerID
+                                  securityIdentity:nil
+                              encryptionPreference:MCEncryptionRequired];
+    _session.delegate = self;
+  }
+  return _session;
+}
+
+- (MCPeerID *)peerID
+{
+  if (!_peerID)
+  {
+    NSAssert(self.displayName, @"No display name. You must initialize this class using the custom intializers.");
+    _peerID = [[MCPeerID alloc] initWithDisplayName:self.displayName];
+  }
+  return _peerID;
+}
+
+- (MCNearbyServiceAdvertiser *)advertiser
+{
+  if (!_advertiser)
+  {
+    NSAssert(self.serviceType, @"No service type`. You must initialize this class using the custom intializers.");
+    _advertiser = [[MCNearbyServiceAdvertiser alloc] initWithPeer:self.peerID
+                                                    discoveryInfo:nil
+                                                      serviceType:self.serviceType];
+    _advertiser.delegate = self;
+  }
+  return _advertiser;
+}
+
+- (MCNearbyServiceBrowser *)browser
+{
+  if (!_browser)
+  {
+    NSAssert(self.serviceType, @"No service type`. You must initialize this class using the custom intializers.");
+    _browser = [[MCNearbyServiceBrowser alloc] initWithPeer:self.peerID
+                                                serviceType:self.serviceType];
+    _browser.delegate = self;
+  }
+  return _browser;
+}
+
+#pragma mark - Session Delegate
+
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state
+{
+  
+}
+
+- (void)session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID
+{
+  
+}
+
+- (void)session:(MCSession *)session didReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID
+{
+  
+}
+
+- (void)session:(MCSession *)session didStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress
+{
+  
+}
+
+- (void)session:(MCSession *)session didFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(NSError *)error
+{
+  
+}
+
+// Required because of an apple bug
+- (void)session:(MCSession *)session didReceiveCertificate:(NSArray *)certificate fromPeer:(MCPeerID *)peerID certificateHandler:(void(^)(BOOL accept))certificateHandler
+{
+  certificateHandler(YES);
+}
+
+#pragma mark - Advertiser Delegate
+
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didReceiveInvitationFromPeer:(MCPeerID *)peerID withContext:(NSData *)context invitationHandler:(void(^)(BOOL accept, MCSession *session))invitationHandler
+{
+  // Just immediately accept all incoming invitations
+  invitationHandler(YES, self.session);
+}
+
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didNotStartAdvertisingPeer:(NSError *)error
+{
+  // TODO: Handle the error
+}
+
+#pragma mark - Browser Delegate
+
+- (void)browser:(MCNearbyServiceBrowser *)browser foundPeer:(MCPeerID *)peerID withDiscoveryInfo:(NSDictionary *)info
+{
+  // Whenever we find a peer, let's just send them an invitation
+  // TODO: Make timeout configurable
+  [browser invitePeer:peerID
+            toSession:self.session
+          withContext:nil
+              timeout:10];
+}
+
+- (void)browser:(MCNearbyServiceBrowser *)browser lostPeer:(MCPeerID *)peerID
+{
+  
+}
+
+- (void)browser:(MCNearbyServiceBrowser *)browser didNotStartBrowsingForPeers:(NSError *)error
+{
+  
+}
+
 
 @end
