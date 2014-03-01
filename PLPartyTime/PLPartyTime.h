9e3870b9a778a65fbeb9b7878089a6781130e555
Peter Livesey <plivesey453@gmail.com>
2014-03-01 12:09:16 -0800

    Initial Commit

---
+++ PLPartyTime/PLPartyTime.h
@@ -0,0 +1,13 @@
+//
+//  PLPartyTime.h
+//  PLPartyTime
+//
+//  Created by Peter Livesey on 3/1/14.
+//  Copyright (c) 2014 Peter Livesey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PLPartyTime : NSObject
+
+@end
---
+++ PLPartyTime/PLPartyTime.m
@@ -0,0 +1,13 @@
+//
+//  PLPartyTime.m
+//  PLPartyTime
+//
+//  Created by Peter Livesey on 3/1/14.
+//  Copyright (c) 2014 Peter Livesey. All rights reserved.
+//
+
+#import "PLPartyTime.h"
+
+@implementation PLPartyTime
+
+@end
